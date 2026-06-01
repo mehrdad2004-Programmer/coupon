a44bdf3a7fa41daf65c480364e0fd00a4187e398
Mehrdad Kalateh Arabi <mehrdad1234kalateh@gmail.com>
2026-06-01 00:16:14 -0700

    (feat): add posts model

--- app/Models/PostsModel.php
+++ app/Models/PostsModel.php
@@ -6,5 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostsModel extends Model
 {
-    //
+    protected $table = "id9j49_posts";
+
+    public $timestamps = false; // WordPress uses its own date columns
+
+    protected $fillable = [
+        "post_author",
+        "post_date",
+        "post_date_gmt",
+        "post_content",
+        "post_title",
+        "post_excerpt",
+        "post_status",
+        "comment_status",
+        "ping_status",
+        "post_password",
+        "post_name",
+        "to_ping",
+        "pinged",
+        "post_modified",
+        "post_modified_gmt",
+        "post_content_filtered",
+        "post_parent",
+        "guid",
+        "menu_order",
+        "post_type",
+        "post_mime_type",
+        "comment_count"
+    ];
+
+    // Optional: Casts for specific columns
+    protected $casts = [
+        "post_author" => "integer",
+        "post_parent" => "integer",
+        "menu_order" => "integer",
+        "comment_count" => "integer",
+        "post_date" => "datetime",
+        "post_date_gmt" => "datetime",
+        "post_modified" => "datetime",
+        "post_modified_gmt" => "datetime"
+    ];
 }
